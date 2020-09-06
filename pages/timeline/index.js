3d2ac12f7221b5ddff5b819740b632661d6df4b5
Karl Behrens <karlbehrensg@gmail.com>
2020-09-06 01:21:49 -0300

    Implementacion de prueba getInitialProps

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import styles from '../../styles/Timelin.module.css'
 
-export default function Timeline () {
+export default function Timeline ({ userName }) {
   return (
     <>
-      <h1 className={styles.h1}>This is the timeline</h1>
+      <h1 className={styles.h1}>This is the timeline of {userName}</h1>
       <Link href='/'>Go home</Link>
     </>
   )
+}
+
+Timeline.getInitialProps = () => {
+  return fetch('http://localhost:3000/api/hello')
+    .then(res => res.json())
 }
