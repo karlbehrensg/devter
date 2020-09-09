a44584311c97864361938362bba0626167a4af0c
Karl Behrens <karlbehrensg@gmail.com>
2020-09-08 22:45:42 -0300

    Extrayendo estilos globales

--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -1,7 +1,5 @@
-import {fonts, colors, breakpoints} from "../../styles/theme";
-import {addOpacityToColor} from "../../styles/utils";
+import styles, {globalStyles} from "./styles";
 
-const backgroundColor = addOpacityToColor(colors.primary, 0.3)
 export default function AppLayout ({children}) {
   return (
     <>
@@ -10,44 +8,8 @@ export default function AppLayout ({children}) {
           {children}
         </main>
       </div>
-      <style jsx>{`
-        div {
-          display: grid;
-          height: 100vh;
-          place-items: center;
-        }
-        
-        main {
-          background: #fff;
-          box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
-          height: 100%;
-          width: 100%;
-        }
-        
-        @media (min-width: ${breakpoints.mobile}) {
-          main {
-            height: 90vh;
-            width: ${breakpoints.mobile};
-          }
-        }
-      `}</style>
-      <style jsx global>{`
-        html,
-        body {
-          background-image: 
-            radial-gradient(${backgroundColor} 1px, transparent 1px),
-            radial-gradient(${backgroundColor} 1px, transparent 1px);
-          background-position: 0 0, 25px 25px;
-          background-size: 50px 50px;
-          padding: 0;
-          margin: 0;
-          font-family: ${fonts.base};
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <style jsx>{styles}</style>
+      <style jsx global>{globalStyles}</style>
     </>
   )
 }
