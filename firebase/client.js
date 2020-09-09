ebb49f60c68ccb4beb508f486a0f6587c581c7a7
Karl Behrens <karlbehrensg@gmail.com>
2020-09-09 02:11:38 -0300

    Autenticacion con GitHub via Firebase

---
+++ firebase/client.js
@@ -0,0 +1,41 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuDXI1mmChpMMO3gwo2E2RqBuouidVhXI",
+  authDomain: "devter-b3d78.firebaseapp.com",
+  databaseURL: "https://devter-b3d78.firebaseio.com",
+  projectId: "devter-b3d78",
+  storageBucket: "devter-b3d78.appspot.com",
+  messagingSenderId: "482645793756",
+  appId: "1:482645793756:web:1be6107904b6236d438d19",
+  measurementId: "G-69MC0KKTKQ"
+};
+
+!firebase.apps.length &&
+firebase.initializeApp(firebaseConfig)
+
+const mapUserFromFirebaseAuthToUser = (user) => {
+  const {displayName, email, photoURL } = user
+
+  return {
+    avatar: photoURL,
+    username: displayName,
+    email
+  }
+}
+
+export const onAuthStateChanged = (onChange) => {
+  return firebase
+    .auth()
+    .onAuthStateChanged(user => {
+      const normalizedUser = mapUserFromFirebaseAuthToUser(user)
+      onChange(normalizedUser)
+    })
+}
+
+export const loginWithGitHub = () => {
+  const githubProvider = new firebase.auth.GithubAuthProvider()
+  return firebase
+    .auth()
+    .signInWithPopup(githubProvider)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
+import {useEffect, useState} from 'react'
 import Head from 'next/head'
 import AppLayout from '../components/AppLayout'
-import {colors} from "../styles/theme";
-import Button from "../components/Button";
-import GitHub from "../components/Icons/GitHub";
+import { colors } from '../styles/theme'
+import Button from '../components/Button'
+import GitHub from '../components/Icons/GitHub'
 
+import {
+  loginWithGitHub,
+  onAuthStateChanged
+} from '../firebase/client'
 
 export default function Home() {
+  const [user, setUser] = useState(undefined)
+
+  useEffect(() => {
+    onAuthStateChanged(setUser)
+  }, [])
+
+  const handleClick = () => {
+    loginWithGitHub().then(setUser).catch(err => {
+      console.log(err)
+    })
+  }
+
   return (
     <>
       <Head>
@@ -15,15 +32,26 @@ export default function Home() {
 
       <AppLayout>
         <section>
-          <img src="/devter-logo.png" alt="Logo"/>
+          <img src='/devter-logo.png' alt='Logo' />
           <h1>Devter</h1>
-          <h2>Talk about development<br />with developers👩‍💻🧑‍💻</h2>
+          <h2>Talk about development<br />with developers 👩‍💻👨‍💻</h2>
+
           <div>
-            <Button>
-              <GitHub fill='#fff' width={24} height={24}/>
-              Login with GitHub
-            </Button>
+            {
+              user === null &&
+              <Button onClick={handleClick}>
+                <GitHub fill='#fff' width={24} height={24} />
+                Login with GitHub
+              </Button>
+            }
+            {
+              user && user.avatar && <div>
+                <img src={user.avatar} />
+                <strong>{user.username}</strong>
+              </div>
+            }
           </div>
+
         </section>
       </AppLayout>
 
@@ -31,24 +59,24 @@ export default function Home() {
         img {
           width: 120px;
         }
-        
+
         div {
           margin-top: 16px;
         }
-        
+
         section {
           display: grid;
           height: 100%;
-          place-items: center;
           place-content: center;
+          place-items: center;
         }
-        
+
         h1 {
           color: ${colors.secondary};
           font-weight: 800;
           margin-bottom: 16px;
         }
-        
+
         h2 {
           color: ${colors.primary};
           font-size: 21px;
