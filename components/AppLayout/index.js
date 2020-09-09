06ff76ce8f78f8c294319dda34e5a389a2d58535
Karl Behrens <karlbehrensg@gmail.com>
2020-09-08 22:25:15 -0300

    Background

--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -1,3 +1,7 @@
+import {fonts, colors} from "../../styles/theme";
+import {addOpacityToColor} from "../../styles/utils";
+
+const backgroundColor = addOpacityToColor(colors.primary, 0.3)
 export default function AppLayout ({children}) {
   return (
     <>
@@ -7,9 +11,14 @@ export default function AppLayout ({children}) {
       <style jsx global>{`
         html,
         body {
+          background-image: 
+            radial-gradient(${backgroundColor} 1px, transparent 1px),
+            radial-gradient(${backgroundColor} 1px, transparent 1px);
+          background-position: 0 0, 25px 25px;
+          background-size: 50px 50px;
           padding: 0;
           margin: 0;
-          font-family: monospace;
+          font-family: ${fonts.base};
         }
 
         * {
---
+++ styles/theme.js
@@ -0,0 +1,8 @@
+export const fonts = {
+  base: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif'
+}
+
+export const colors = {
+  primary: '#0099ff',
+  secondary: '#1c5480'
+}
